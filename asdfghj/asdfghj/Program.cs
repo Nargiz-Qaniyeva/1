c9464c3eb55cb4452599f13f7b6df6396ba401a7
Nargiz-Qaniyeva <tu7lyw21z@code.edu.az>
2022-03-08 21:25:55 +0400

    first commit

---
+++ asdfghj/asdfghj/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace asdfghj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+
+
+            int math = division(36,6);
+            Console.WriteLine(math);
+           
+            static int division(int num1, int num2)
+            {
+                return num1 / num2;
+            }
+
+            #endregion
+        }
+       
+    }
+    }
+
